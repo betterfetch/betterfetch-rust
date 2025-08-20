5895f626334f7ba0125f245da24069fb8efb1e9a
David Balishyan <davidbalishyan12@gmail.com>
2025-08-20 21:32:46 +0400

    Added: version flag

--- src/args.rs
+++ src/args.rs
@@ -1,7 +1,11 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(name = "betterfetch", about = "System fetcher written in Rust")]
+#[command(
+    name = "betterfetch",
+    about = "System fetcher written in Rust.",
+    version = env!("CARGO_PKG_VERSION"),
+)]
 pub struct Args {
     // Hide ASCII art
     #[arg(short, long)]
